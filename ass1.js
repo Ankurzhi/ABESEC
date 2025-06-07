16a4be4524a84c988b92ea71afc9c3571e77ef81
Ankurzhi <132478524+Ankurzhi@users.noreply.github.com>
2025-06-07 17:27:42 +0000

    Updated ABES.html content

--- ass1.js
+++
@@ -1,9 +0,0 @@
-function table()
-{
-    let a=parseInt(document.getElementById("tab").value);
-    for(i=1;i<=10;i++)
-    {
-        var c=a*1;
-        console.log(c);
-    }
-}
--- newjs.js
+++
@@ -1,8 +0,0 @@
-console.log(innerHeight);
-console.log(innerWidth);
-function abc()
-{
-    let x=window.open("https://www.codershelpline.com/",self,height=200,width=300);
-    
-    
-}
